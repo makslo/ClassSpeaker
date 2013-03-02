7ccb86525669707c2a01abae2d4a8a6feb5018fa
Max <max@Maxs-MacBook-Air.local>
2013-03-02 10:06:26 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClassSpeaker::Application.config.secret_token = '5e641c4690c13ed8374dbadc44d3c100e8f036329e3f24fafd09e397698c652109f61ea3d12f4fde3ceb3995d8a94f0b39c3a2b4f38a7be3639702eb612adb3c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ClassSpeaker::Application.config.session_store :cookie_store, key: '_ClassSpeaker_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ClassSpeaker::Application.config.session_store :active_record_store
