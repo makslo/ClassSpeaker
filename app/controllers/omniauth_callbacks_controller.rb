0a79c8b0201b4988d7e0035aa0e40110d4e37cfa
Max <max@Maxs-MacBook-Air.local>
2013-03-03 03:49:12 -0500

    update

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,10 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	def linkedin
+		render :text=> request.env["omniauth.auth"].to_yaml
+		
+	end
+
+	def tmp
 		user = User.from_omniauth(request.env["omniauth.auth"])
 		if user.persisted?
 	      flash.notice = "Signed in!"
